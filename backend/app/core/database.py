4a7d7f96c4d72fb745cd8b66e56ce3de6dd073c2
muralirevuri07-boop <muralirevuri07@gmail.com>
2026-07-02 13:13:33 +0530

    Print database URL

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+print("DATABASE_URL FROM SETTINGS:", settings.DATABASE_URL)
+
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
